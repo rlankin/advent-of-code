3176a865e7a89d6f9ee8e567a24dba54b09859db
Ryan Lankin <rlankin@solutionary.com>
2015-12-25 12:08:59 -0600

    Day 25 (part 1)

---
+++ day25.py
@@ -0,0 +1,30 @@
+"""
+Puzzle input:
+To continue, please consult the code grid in the manual.  Enter the code at row 2947, column 3029.
+"""
+
+def get_code(row, col):
+    code = 20151125
+    r = 2
+
+    while True:
+        r_i = r
+        c_i = 1
+
+        while r_i > 0:
+            code = (code * 252533) % 33554393
+
+            if r_i == row and c_i == col:
+                return code
+
+            #print("({0}, {1}) = {2}".format(r_i, c_i, code))
+            r_i -= 1
+            c_i += 1
+
+        r += 1
+
+def part_1():
+    code = get_code(2947, 3029)
+    print("Part 1: " + str(code))
+
+part_1()
