7d496acee0d71c8f3fd929000fbb78e3105294f0
Ryan Lankin <rlankin@gmail.com>
2016-12-17 01:03:19 -0600

    Day 17

---
+++ 2016/day17.py
@@ -0,0 +1,63 @@
+import hashlib
+
+PASSCODE = 'vwbaicqe'
+OPEN_VALUES = ['b', 'c', 'd', 'e', 'f']
+DESTINATION = [3, 3]
+
+def get_open_doors(passcode, path):
+    open_doors = []
+    values = hashlib.md5((passcode + path).encode('utf-8')).hexdigest()
+
+    if values[0] in OPEN_VALUES:
+        open_doors += 'U'
+    if values[1] in OPEN_VALUES:
+        open_doors += 'D'
+    if values[2] in OPEN_VALUES:
+        open_doors += 'L'
+    if values[3] in OPEN_VALUES:
+        open_doors += 'R'
+
+    return open_doors
+
+def at_destination(path, destination):
+    position = [0, 0]
+
+    for d in path:
+        if d == 'U':
+            position[1] -= 1
+        elif d == 'D':
+            position[1] += 1
+        elif d == 'L':
+            position[0] -= 1
+        elif d == 'R':
+            position[0] += 1
+
+    return position == destination
+
+def get_paths(x, y, path):
+    if [x, y] == DESTINATION:
+        return [path]
+
+    paths = []
+    for door in get_open_doors(PASSCODE, path):
+        if door == 'U' and y - 1 >= 0:
+            paths += get_paths(x, y - 1, path + door)
+        if door == 'D' and y + 1 < 4:
+            paths += get_paths(x, y + 1, path + door)
+        if door == 'L' and x - 1 >= 0:
+            paths += get_paths(x - 1, y, path + door)
+        if door == 'R' and x + 1 < 4:
+            paths += get_paths(x + 1, y, path + door)
+
+    return paths
+
+def part_1():
+    paths = get_paths(0, 0, '')
+    print('Part 1: {}'.format(min(paths, key = len)))
+
+def part_2():
+    paths = get_paths(0, 0, '')
+    print('Part 2: {}'.format(len(max(paths, key = len))))
+
+part_1()
+part_2()
