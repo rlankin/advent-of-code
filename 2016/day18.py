9692020591582b6173cf51246665ff0b750f8032
Ryan Lankin <rlankin@gmail.com>
2016-12-18 00:11:05 -0600

    Day 18

---
+++ 2016/day18.py
@@ -0,0 +1,47 @@
+# TEST_INPUT = '.^^.^.^^^^'
+FIRST_ROW = '.^^.^^^..^.^..^.^^.^^^^.^^.^^...^..^...^^^..^^...^..^^^^^^..^.^^^..^.^^^^.^^^.^...^^^.^^.^^^.^.^^.^.'
+
+# TODO: Simplify expression? Boolean algebra? Some other trickery?
+def get_tile(left, center, right):
+    """
+        Its left and center tiles are traps, but its right tile is not.
+        Its center and right tiles are traps, but its left tile is not.
+        Only its left tile is a trap.
+        Only its right tile is a trap.
+    """
+    is_trap = (left == '^' and center == '^' and right == '.') \
+        or (left == '.' and center == '^' and right == '^') \
+        or (left == '^' and center == '.' and right == '.') \
+        or (left == '.' and center == '.' and right == '^')
+    return '^' if is_trap else '.'
+
+def get_next_row(row):
+    next_row = ''
+
+    for i in range(len(row)):
+        next_row += get_tile(row[i - 1] if i > 0 else '.', row[i], row[i + 1] if i < len(row) - 1 else '.')
+
+    return next_row
+
+def part_1():
+    room = [FIRST_ROW]
+    safe_tiles = sum([1 for tile in room[0] if tile == '.'])
+
+    for i in range(39):
+        room.append(get_next_row(room[-1]))
+        safe_tiles += sum([1 for tile in room[-1] if tile == '.'])
+
+    print('Part 1: {}'.format(safe_tiles))
+
+def part_2():
+    room = [FIRST_ROW]
+    safe_tiles = sum([1 for tile in room[0] if tile == '.'])
+
+    for i in range(399999):
+        room.append(get_next_row(room[-1]))
+        safe_tiles += sum([1 for tile in room[-1] if tile == '.'])
+
+    print('Part 2: {}'.format(safe_tiles))
+
+part_1()
+part_2()
