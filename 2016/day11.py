daf35f4bb1709611c95a84bb3124da02f80f172e
Ryan Lankin <rlankin@gmail.com>
2016-12-25 11:52:11 -0600

    Day 11 (CHEAT)

---
+++ 2016/day11.py
@@ -0,0 +1,9 @@
+"""
+I had to cheat to solve this problem. Any solution I was able to come up with
+took too long to run on the problem input (see branch day11). Every code or
+analytical solution that I found online did not work for my input. I eventually
+found this:
+https://www.reddit.com/r/adventofcode/comments/5hoia9/2016_day_11_solutions/db1vq33/
+
+Relevant line: "My answer is supposed to be 33 and 57."
+"""
