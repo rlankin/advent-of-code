b8ad185a7fcb52ab6bd5c164c785ed3d1c334e8c
Ryan Lankin <rlankin@gmail.com>
2016-12-25 02:12:16 -0600

    Day 11 - BFS

--- 2016/day11.py
+++ 2016/day11.py
@@ -4,59 +4,70 @@ def print_floors(floors):
     for floor_num in range(len(floors) - 1, -1, -1):
         print('F{} {}'.format(floor_num, ' '.join(floors[floor_num])))
 
-def get_moves(elevator_loc, floors):
-    moves = []
-
-    for d in [-1, 1]:
-        if 0 <= elevator_loc + d < len(floors):
-            for i, item in enumerate(floors[elevator_loc]):
-                moves.append((d, [item]))
-
-                for item2 in floors[elevator_loc][i + 1:]:
-                    moves.append((d, [item, item2]))
-
-    return moves
-
-def is_valid_state(floors):
-    for floor in floors:
+def is_valid_state(state):
+    for floor in state[0]:
         for item in floor:
-            if item[-1] == 'M' and item[:-1] not in [i[:-1] for i in floor]:
+            if item[-1] == 'M' and item[:-1] + 'G' not in floor \
+                and len([i for i in floor if i[-1] == 'G']) > 0:
                 return False
 
     return True
 
-def move(elevator_loc, floors, moves):
-    if not is_valid_state(floors):
-        return -1
+def get_moves(state):
+    moves = []
+    elevator_loc = state[1]
 
-    if sum([len(f) for f in floors[:-1]]) > 0:
-        for m in get_moves(elevator_loc, floors):
-            # Perform move
-            new_elevator_loc = elevator_loc + m[0]
-            new_floors = copy.deepcopy(floors)
-            new_floors[new_elevator_loc] += m[1]
-            # TODO: Is there an easier/better way to remove items like this?
-            new_floors[elevator_loc] = [i for i in new_floors[elevator_loc] if i not in m[1]]
+    for d in [-1, 1]:
+        if 0 <= elevator_loc + d < len(state[0]):
+            for i, item in enumerate(state[0][elevator_loc]):
+                move = copy.deepcopy(state)
+                move[0][elevator_loc + d].append(item)
+                move[0][elevator_loc].remove(item)
+                if is_valid_state(move):
+                    move[1] += d
+                    move[2] += 1
+                    moves.append(move)
 
-            # Continue looking for solutions
-            sub_moves = move(new_elevator_loc, new_floors, moves + 1)
-            if sub_moves != -1 and sub_moves < moves:
-                moves = sub_moves
+                for item2 in state[0][elevator_loc][i + 1:]:
+                    move = copy.deepcopy(state)
+                    move[0][elevator_loc + d] += [item, item2]
+                    move[0][elevator_loc].remove(item)
+                    move[0][elevator_loc].remove(item2)
+                    if is_valid_state(move):
+                        move[1] += d
+                        move[2] += 1
+                        moves.append(move)
 
     return moves
 
-def part_1():
-    # floors = [['PmG', 'PmM'],
-    #     ['CoG', 'CmG', 'RuG', 'PuG'],
-    #     ['CoM', 'CmM', 'RuM', 'PuM'],
-    #     []]
-    floors = [['HM', 'LiM'],
-        ['HG'],
-        ['LiG'],
-        []]
+def get_min_steps(state):
+    state_hist = [(state[0], state[1])]
+    moves = get_moves(state)
 
-    min_moves = move(0, floors, -1)
-    print(min_moves)
-    # print(get_moves(3, floors))
+    while len(moves) > 0:
+        move = moves.pop(0)
+
+        if (move[0], move[1]) in state_hist:
+            continue
+
+        if sum([len(floor) for floor in move[0][:-1]]) == 0:
+            return move[2]
+
+        state_hist.append((move[0], move[1]))
+
+        moves += get_moves(move)
+
+def part_1():
+    floors = [['PmG', 'PmM'],
+        ['CoG', 'CmG', 'RuG', 'PuG'],
+        ['CoM', 'CmM', 'RuM', 'PuM'],
+        []]
+    # floors = [['HM', 'LiM'],
+    #     ['HG'],
+    #     ['LiG'],
+    #     []]
+
+    min_steps = get_min_steps([floors, 0, 0])
+    print('Part 1: {}'.format(min_steps))
 
 part_1()
