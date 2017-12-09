eebcfe0fa6704456d063138072868316bc19cce0
Ryan Lankin <rlankin@gmail.com>
2017-12-09 00:03:10 -0600

    Day 9 (improved)

--- 2017/day9.py
+++ 2017/day9.py
@@ -5,33 +5,27 @@ with open('input/day9_input', 'r') as f:
 score = 0
 group_level = 0
 garbage_chars = 0
-garbage = False
-ignore = False
 
-for c in stream:
-    if ignore:
-        ignore = False
-        continue
-
-    if c == '!':
-        garbage_chars -= 1
-        ignore = True
-    elif c == '<':
-        if not garbage:
-            garbage_chars -= 1
-        garbage = True
-    elif c == '>':
-        garbage = False
-
-    if garbage:
-        garbage_chars += 1
-        continue
-
-    if c == '{':
+i = 0
+while i < len(stream):
+    if stream[i] == '!':
+        i += 1
+    elif stream[i] == '<':
+        while i < len(stream):
+            i += 1
+            if stream[i] == '!':
+                i += 1
+            elif stream[i] == '>':
+                break
+            else:
+                garbage_chars += 1
+    elif stream[i] == '{':
         group_level += 1
-    elif c == '}':
+    elif stream[i] == '}':
         score += group_level
         group_level -= 1
+
+    i += 1
 
 print('Part 1: {}'.format(score))
 print('Part 2: {}'.format(garbage_chars))
